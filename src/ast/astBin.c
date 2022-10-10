a7b830328133c3f9045f85c3565276007058bc14
Sasha <drdy.business@gmail.com>
2022-10-11 00:56:13 +0300

    Removed the AssignmentPrint from astBin

--- src/ast/astBin.c
+++ src/ast/astBin.c
@@ -1,19 +1,10 @@
 #include "../include/ast/astBin.h"
 
-void AssignmentPrint(astBin_t* self, size_t indent);
-
 void AstBin(astBin_t* self, astType_t type, ast_t* left, ast_t* right) {
     Ast(&self->base, type);
 
     self->left = left;
     self->right = right;
-
-    switch (type) {
-        case AST_ASSIGNMENT:
-            self->base.Print = (void (*)(ast_t*, size_t)) AssignmentPrint;
-            break;
-        default: break;
-    }
 }
 
 astBin_t* newAstBin(astType_t type, ast_t* left, ast_t* right) {
@@ -22,17 +13,4 @@ astBin_t* newAstBin(astType_t type, ast_t* left, ast_t* right) {
 
     AstBin(self, type, left, right);
     return self;
-}
-
-void AssignmentPrint(astBin_t* self, size_t indent) {
-    INDENT(indent)
-    printf("%s\n", astTypeToStr(self->base.type));
-    
-    INDENT(indent + 1)
-    printf("left:\n");
-    self->left->Print(self->left, indent + 2);
-    
-    INDENT(indent + 1)
-    printf("right:\n");
-    self->right->Print(self->right, indent + 2);
 }
