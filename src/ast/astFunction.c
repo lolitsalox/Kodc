6c06fe6994cef31d74478d8330fdd35dffec61a1
Sasha <drdy.business@gmail.com>
2022-10-10 03:14:12 +0300

    Implemented Print methods

--- src/ast/astFunction.c
+++ src/ast/astFunction.c
@@ -1,8 +1,10 @@
 #include "../include/ast/astFunction.h"
 
+void FunctionPrint(astFunction_t* self, size_t indent);
+
 void AstFunction(astFunction_t* self) {
     AstCompound(&self->base);
-    // self->base.base.Print = (void (*)(ast_t*, size_t)) FunctionPrint;
+    self->base.base.Print = (void (*)(ast_t*, size_t)) FunctionPrint;
     self->base.base.type = AST_FUNCTION;
     self->base.base.dtypeInfo.dtype = DTYPE_U64;
     
@@ -20,4 +22,31 @@ astFunction_t* newAstFunction() {
 
     AstFunction(self);
     return self;
+}
+
+void FunctionPrint(astFunction_t* self, size_t indent) {
+    INDENT(indent)
+    printf("%s\n", astTypeToStr(self->base.base.type));
+    
+    INDENT(indent + 1)
+    printf("name: %s\n", self->name);
+    
+    INDENT(indent + 1)
+    printf("return type: ");
+
+    for (size_t i = 0; i < self->returnTypeInfo.ptrCount; ++i)
+        printf("#");
+    printf("%s\n", dTypeToStr(self->returnTypeInfo.dtype));
+    
+    INDENT(indent + 1)
+    printf("Parameters:\n");
+
+    for (size_t i = 0; i < self->base.children->size; ++i) {
+        // INDENT(indent + 1)
+        // printf("Parameter #%lld:\n", i);
+        ast_t* child = (ast_t*) self->base.children->items[i];
+        child->Print(child, indent + 2);
+    }
+
+    ((ast_t*)self->body)->Print((ast_t*)self->body, indent + 1);
 }
--- src/ast/astVariable.c
+++ src/ast/astVariable.c
@@ -1,8 +1,10 @@
 #include "../include/ast/astVariable.h"
 
+void VariablePrint(astVariable_t* self, size_t indent);
+
 void AstVariable(astVariable_t* self, char* name) {
     Ast(&self->base, AST_VARIABLE);
-    // self->base.Print = (void (*)(ast_t*, size_t)) VariablePrint;
+    self->base.Print = (void (*)(ast_t*, size_t)) VariablePrint;
 
     self->name = name;
     self->stackIndex = 0;
@@ -14,4 +16,18 @@ astVariable_t* newAstVariable(char* name) {
 
     AstVariable(self, name);
     return self;
+}
+
+void VariablePrint(astVariable_t* self, size_t indent) {
+    INDENT(indent)
+    printf("%s\n", astTypeToStr(self->base.type));
+    
+    INDENT(indent + 1)
+    printf("name: %s\n", self->name);
+
+    INDENT(indent + 1);
+    printf("type: ");
+    for (size_t i = 0; i < self->base.dtypeInfo.ptrCount; ++i)
+        printf("#");
+    printf("%s\n", dTypeToStr(self->base.dtypeInfo.dtype));
 }
