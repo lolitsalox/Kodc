a316b6229860dc8fcdb398f9674249aec3ec0a5b
lolitsalox <drdy.business@gmail.com>
2022-10-12 22:50:39 +0300

    Implemented print

--- src/ast/astUnaryOp.c
+++ src/ast/astUnaryOp.c
@@ -1,7 +1,10 @@
 #include "../include/ast/astUnaryOp.h"
 
+void AstUnaryOpPrint(astUnaryOp_t* self, size_t indent);
+
 void AstUnaryOp(astUnaryOp_t* self, tokenType_t op, ast_t* value) {
     Ast(&self->base, AST_UNARY_OP);
+    self->base.Print = (void (*)(ast_t*, size_t)) AstUnaryOpPrint;
 
     self->op = op;
     self->value = value;
@@ -13,4 +16,13 @@ astUnaryOp_t* newAstUnaryOp(tokenType_t op, ast_t* value) {
 
     AstUnaryOp(self, op, value);
     return self;
+}
+
+void AstUnaryOpPrint(astUnaryOp_t* self, size_t indent) {
+    INDENT(indent)
+    printf("%s - %s\n", astTypeToStr(self->base.type), tokenTypeToStr(self->op));
+
+    INDENT(indent + 1)
+    printf("value:\n");
+    self->value->Print(self->value, indent + 2);
 }
