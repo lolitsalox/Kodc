4f2bc3f6d670abe894ae9c80f4af5b26dc5d6f16
lolitsalox <drdy.business@gmail.com>
2022-10-12 22:51:04 +0300

    Updated functions

--- src/ast/astCall.c
+++ src/ast/astCall.c
@@ -1,17 +1,17 @@
 #include "../include/ast/astCall.h"
 
-void AstCall(astCall_t* self, char* name, astCompound_t* arguments) {
+void AstCall(astCall_t* self, ast_t* value, astCompound_t* arguments) {
     Ast(&self->base, AST_CALL);
     // self->base.Print = (void (*)(ast_t*, size_t)) CallPrint;
 
     self->arguments = arguments;
-    self->name = name;
+    self->value = value;
 }
 
-astCall_t* newAstCall(char* name, astCompound_t* arguments) {
+astCall_t* newAstCall(ast_t* value, astCompound_t* arguments) {
     astCall_t* self = (astCall_t*) malloc(sizeof(astCall_t));
     if (!self) error_malloc(__func__);
 
-    AstCall(self, name, arguments);
+    AstCall(self, value, arguments);
     return self;
 }
